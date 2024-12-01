e6a5214cc468b4ba72effae3dc0311d9efea1482
Alan Davies <alan@alandavies.org>
2024-12-01 12:51:56 -0500

    Cleaning up

--- day1/day1.py
+++ day1/day1.py
@@ -1,45 +1,38 @@
 import sys
 
 
-def part_1(file_path):
-    with open(file_path, 'r') as lines:
-        rows = (line.rstrip('\n') for line in lines)
-        list1 = []
-        list2 = []
-        for row in rows:
-            elements = row.split()
-            list1.append(int(elements[0]))
-            list2.append(int(elements[1]))
-        list1.sort()
-        list2.sort()
-        total = 0
-        for (item1, item2) in zip(list1, list2):
-            distance = abs(item1 - item2)
-            total += distance
-        print(total)
+def part_1(list1, list2):
+    list1.sort()
+    list2.sort()
+    total = 0
+    for (item1, item2) in zip(list1, list2):
+        distance = abs(item1 - item2)
+        total += distance
+    return total
 
 
-def part_2(file_path):
+def part_2(list1, list2):
+    number_counts = {}
+    total = 0
+    for number in list1:
+        count = number_counts.get(number, -1)
+        if count == -1:
+            count = list2.count(number)
+            number_counts[number] = count
+        total += number * count
+    return total
+
+
+def part_n(file_path, fn):
     with open(file_path, "r") as lines:
-        rows = (line.rstrip("\n") for line in lines)
         list1 = []
         list2 = []
-        for row in rows:
-            elements = row.split()
-            list1.append(int(elements[0]))
-            list2.append(int(elements[1]))
-        list1.sort()
-        list2.sort()
-
-        number_counts = {}
-        total = 0
-        for number in list1:
-            count = number_counts.get(number, -1)
-            if count == -1:
-                count = list2.count(number)
-                number_counts[number] = count
-            total += number * count
-        print(total)
+        for row in (line.rstrip("\n") for line in lines):
+            elements = [int(x) for x in row.split()]
+            list1.append(elements[0])
+            list2.append(elements[1])
+        answer = fn(list1, list2)
+        print(answer)
 
 
 if len(sys.argv) != 3:
@@ -50,9 +43,9 @@ part = sys.argv[1]
 file_path = sys.argv[2]
 
 if part == "1":
-    part_1(file_path)
+    part_n(file_path, part_1)
 elif part == "2":
-    part_2(file_path)
+    part_n(file_path, part_2)
 else:
     print("Unknown part")
     exit(1)
