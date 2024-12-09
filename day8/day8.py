2dbe196bd4f32699b7f4082742e249c347f552d2
Alan Davies <alan@alandavies.org>
2024-12-08 20:13:24 -0500

    Full day 8 solution

--- day8/day8.py
+++ day8/day8.py
@@ -19,75 +19,103 @@ def make_empty_grid(grid):
     return grid2
 
 
-def prune_out_of_bounds(coordinates_pair, grid):
-    # print(f"check_bounds: {coordinates_pair=}")
+def project_antinodes(start_r, start_c, dr, dc, dir_r, dir_c, num_rows, num_cols, num_projections):
+    result = []
+    n = 1
+    while True:
+        r, c = start_r + dir_r * n * dr, start_c + dir_c * n * dc
+        if r < 0 or r >= num_rows or c < 0 or c >= num_cols: break
+        result.append((r, c))
+        if n == num_projections: break
+        n = n + 1
+    return result
+
+
+def calc_antinodes(part, antenna_coordinates_pair, grid):
+    print(f"calc_antinodes: {antenna_coordinates_pair=}")
     num_rows = len(grid)
     num_cols = len(grid[0]) if num_rows > 0 else 0
-    return [(r, c) for (r, c) in coordinates_pair if r >= 0 and r < num_rows and c >=0 and c < num_cols]
 
-
-def calc_antinodes(antenna_coordinates_pair, grid):
-    print(f"calc_antinodes: {antenna_coordinates_pair=}")
     (r1, c1), (r2, c2) = antenna_coordinates_pair
     dr, dc = abs(r2 - r1), abs(c2 - c1)
+    
+    result = []
+
+    # For part 1 we want to only project one antinode either side of the aligned pair of antennas
+    # For part 2 we project out to the edges of the grid
+    num_projections = 1 if part == 1 else -1
+    
+    # For part 2 we also consider the antennas to be antinodes
+    if part == 2: result.extend(antenna_coordinates_pair)
+
     if r1 < r2:
         if c1 > c2:
             #    #
             #   1
             #  2
             # #
-            return prune_out_of_bounds(((r1 - dr, c1 + dc), (r2 + dr, c2 - dc)), grid)
+            result.extend(project_antinodes(r1, c1, dr, dc, -1, 1, num_rows, num_cols, num_projections))
+            result.extend(project_antinodes(r2, c2, dr, dc, 1, -1, num_rows, num_cols, num_projections))
         else:
             # #
             #  1
             #   2
             #    #
-            return prune_out_of_bounds(((r1 - dr, c1 - dc), (r2 + dr, c2 + dc)), grid)
+            result.extend(project_antinodes(r1, c1, dr, dc, -1, -1, num_rows, num_cols, num_projections))
+            result.extend(project_antinodes(r2, c2, dr, dc, 1, 1, num_rows, num_cols, num_projections))
     else:
         if c1 > c2:
             #    #
             #   2
             #  1
             # #
-            return prune_out_of_bounds(((r1 + dr, c1 - dc), (r2 - dr, c2 + dc)), grid)
+            result.extend(project_antinodes(r1, c1, dr, dc, 1, -1, num_rows, num_cols, num_projections))
+            result.extend(project_antinodes(r2, c2, dr, dc, -1, 1, num_rows, num_cols, num_projections))
         else:
             # #
             #  2
             #   1
             #    #
-            return prune_out_of_bounds(((r1 + dr, c1 + dc), (r2 - dr, c2 - dc)), grid)
+            result.extend(project_antinodes(r1, c1, dr, dc, 1, 1, num_rows, num_cols, num_projections))
+            result.extend(project_antinodes(r2, c2, dr, dc, -1, -1, num_rows, num_cols, num_projections))
+
+    return result
 
 
-def part_1(grid):
+def calc(grid, part):
     print_grid(grid)
     print()
     antinodes = set()
-    
+
     antennas = defaultdict(list)
     for r, row in enumerate(grid):
         for c, cell in enumerate(row):
-            if cell == '.': continue
+            if cell == ".":
+                continue
             antennas[cell].append((r, c))
 
     for name, antenna_coordinates_list in antennas.items():
         for antenna_coordinates_pair in combinations(antenna_coordinates_list, 2):
-            antinodes_coordinates_list = calc_antinodes(antenna_coordinates_pair, grid)
+            antinodes_coordinates_list = calc_antinodes(part, antenna_coordinates_pair, grid)
             grid2 = make_empty_grid(grid)
-            for (r, c) in antenna_coordinates_pair:
+            for r, c in antenna_coordinates_pair:
                 grid2[r][c] = name
-            for (r, c) in antinodes_coordinates_list:
-                grid[r][c] = "#"
-                grid2[r][c] = "#"
+            for r, c in antinodes_coordinates_list:
+                grid2[r][c] = grid[r][c] = "#"
                 antinodes.add((r, c))
             print_grid(grid2)
             print()
-            
+
     print_grid(grid)
     return len(antinodes)
 
 
-def part_2(equations):
-    return 2
+def part_1(grid):
+    return calc(grid, 1)
+
+
+def part_2(grid):
+    return calc(grid, 2)
 
 
 def part_n(file_path, fn):
